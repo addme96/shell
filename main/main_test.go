9d829583170d7ee27210faa16bba4c591d5e8399
Dawid_Dudek <Dawid_Dudek@epam.com>
2021-02-10 17:32:45 +0100

    Test_printProperMessage ready

--- main/main_test.go
+++ main/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -12,7 +13,6 @@ func Test_printProperMessage(t *testing.T) {
 		minMsg:  "tock",
 		hourMsg: "bong",
 	}
-
 	type args struct {
 		messages *Messages
 		i        int
@@ -33,40 +33,56 @@ func Test_printProperMessage(t *testing.T) {
 		{"minutes 1 minute", args{messages, 60}, expectedForMinutes},
 		{"minutes 2 minutes", args{messages, 120}, expectedForMinutes},
 		{"minutes 4 minutes", args{messages, 240}, expectedForMinutes},
-
 		{"hours 1 hour", args{messages, 3600}, expectedForHours},
 		{"hours 2 hours", args{messages, 2 * 3600}, expectedForHours},
 		{"hours 10 hours", args{messages, 10 * 3600}, expectedForHours},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// arrange
 			buf := bytes.Buffer{}
+			// act
 			printProperMessage(tt.args.messages, tt.args.i, &buf)
+			// assert
 			assert.Equal(t, tt.expected, buf.String())
 		})
 	}
 }
 
-//
-//func Test_readConfig(t *testing.T) {
-//	type args struct {
-//		reader io.Reader
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want Messages
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if got := readConfig(tt.args.reader); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("readConfig() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func Test_readConfig(t *testing.T) {
+	messages1 := Messages{
+		secMsg:  "tic",
+		minMsg:  "tac",
+		hourMsg: "toe",
+	}
+	messages2 := Messages{
+		secMsg:  "tick",
+		minMsg:  "tock",
+		hourMsg: "bong",
+	}
+
+	tests := []struct {
+		name             string
+		config           string
+		expectedMessages Messages
+	}{
+		{"tic tac toe", "tic\ntac\ntoe\n", messages1},
+		{"tick tock bong", "tick\ntock\nbong\n", messages2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arrange
+			reader := strings.NewReader(tt.config)
+			// act
+			actualMessages := readConfig(reader)
+			// assert
+			assert.Equal(t, tt.expectedMessages.secMsg, actualMessages.secMsg)
+			assert.Equal(t, tt.expectedMessages.minMsg, actualMessages.minMsg)
+			assert.Equal(t, tt.expectedMessages.hourMsg, actualMessages.hourMsg)
+		})
+	}
+}
+
 //
 //func Test_tickClock(t *testing.T) {
 //	type args struct {
